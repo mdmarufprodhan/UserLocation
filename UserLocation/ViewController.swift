61c624820e2bbc9593c2643f7933d51732a3fdd5
Md Maruf Prodhan <mdmarufprodhan@Mds-Mac.local>
2020-02-07 10:40:09 -0800

    Initial Commit

---
+++ UserLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserLocation
+//
+//  Created by Md Maruf Prodhan on 2/7/20.
+//  Copyright © 2020 Md Maruf Prodhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
